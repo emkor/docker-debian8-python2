1491ea559a3a3893db4ea03141214c20bd5c0ddd
mkorzeni <mateusz.korzeniowski@nokia.com>
2017-01-07 12:51:27 +0100

    removed unused test, uncommented additional dependency for cryptography

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 # run container instance with test data mounted at /test
 
-# test for red-build, non-0 code shall be returned
+# test python installation
 docker run -it --name "test-debian8-python2" -v $(pwd)/test:/test -w /test endlessdrones/debian8-python2 bash ./execute_python2.sh
-docker stop "test-debian8-python2" && docker rm "test-debian8-python2"
-
-# install pip modules
-docker run -it --name "test-debian8-python2" -v $(pwd)/test:/test -w /test endlessdrones/debian8-python2 bash ./install_top_modules.sh
 docker stop "test-debian8-python2" && docker rm "test-debian8-python2"
--- test/install_top_modules.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# verify if docker image OS contains dependencies for installing most popular PyPI packages
-pip install -rrequirements.txt
-python top_modules_import.py &> /dev/null || exit 1
