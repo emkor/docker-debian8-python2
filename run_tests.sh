83537e5d346fba89a421d28329bd881dba6ed020
mkorzeni <mateusz.korzeniowski@nokia.com>
2017-01-07 11:59:34 +0100

    extended scripts

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-
 # run container instance with test data mounted at /test
+
+# test for red-build, non-0 code shall be returned
+docker run -it --name "test-debian8-python2" -v $(pwd)/test:/test -w /test endlessdrones/debian8-python2 bash ./execute_python2.sh
+docker stop "test-debian8-python2" && docker rm "test-debian8-python2"
+
+# install custom modules
 docker run -it --name "test-debian8-python2" -v $(pwd)/test:/test -w /test endlessdrones/debian8-python2 bash ./install_top_modules.sh
 docker stop "test-debian8-python2" && docker rm "test-debian8-python2"
 
-# execute test scripts
-#docker exec "test-debian8-python2" cd /test && bash ./insall_top_modules.sh && cd ..
