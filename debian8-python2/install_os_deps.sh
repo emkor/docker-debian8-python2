9582fe40b9d14121b4c3abdd000a2824671aad4f
mkorzeni <mateusz.korzeniowski@nokia.com>
2017-07-07 23:22:57 +0200

    added native-C mysql client and gcc

--- debian8-python2/install_os_deps.sh
+++ debian8-python2/install_os_deps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y wget build-essential zlib1g-dev libssl-dev libffi-dev
+apt-get install -y wget build-essential zlib1g-dev libssl-dev libffi-dev libmysqlclient-dev gcc
